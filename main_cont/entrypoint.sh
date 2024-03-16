b0289547afb16dc709110a53cdb5d1db3c6876de
Diana <dianadumskaia@yandex.ru>
2024-03-16 13:42:27 +0300

    начинает реализовывать elk

--- main_cont/entrypoint.sh
+++ main_cont/entrypoint.sh
@@ -1,28 +1,16 @@
 #!/bin/bash
 
-wait_for_elastic_node_1() {
+wait_for_elasticsearch() {
   tries=1
   echo "Waiting for Elastisearch node 1..."
-  until curl "elasticsearch-node1:$ELASTIC_PORT/_cluster/health?wait_for_status=yellow&timeout=30s"; do
+  until curl "$ELASTIC_HOST:$ELASTIC_PORT/_cluster/health?wait_for_status=yellow&timeout=30s"; do
     >&2 echo "Elastisearch is unavailable - waiting for it... 😴 ($tries)"
     sleep 1
     tries=$(expr $tries + 1)
   done
-  echo "Elasticsearch node 1 is ready!"
+  echo "Elasticsearch is ready!"
 }
 
-wait_for_elastic_node_2() {
-  tries=1
-  echo "Waiting for Elastisearch node 2..."
-  until curl "elasticsearch-node2:$ELASTIC_PORT/_cluster/health?wait_for_status=yellow&timeout=30s"; do
-    >&2 echo "Elastisearch is unavailable - waiting for it... 😴 ($tries)"
-    sleep 1
-    tries=$(expr $tries + 1)
-  done
-  echo "Elasticsearch node 2 is ready!"
-}
-
-wait_for_elastic_node_1
-wait_for_elastic_node_2
+wait_for_elasticsearch
 
 uvicorn main:app --proxy-headers --host "$MAIN_HOST" --port "$MAIN_PORT"
